4cefe358b6e09a08310031cacb4f01be46fc4ff7
IamPersistent <develop@zestic.com>
2011-11-19 01:35:27 -0800

    add User stub class

---
+++ src/Facebook/Graph/User.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * MIT license
+ */
+namespace Facebook\Graph;
+
+use Facebook\Graph\Owner;
+/**
+ * @author Richard Shank <develop@zestic.com>
+ */
+class User extends Owner
+{
+    // TODO:
+}
